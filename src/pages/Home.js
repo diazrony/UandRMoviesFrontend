d25f54ec3300fb504ace1624dbd22bc7282b55cd
Jesus Uriel Garrido Granados <urielgarridogra@gmail.com>
2020-11-23 23:52:41 -0600

    Listo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../components/styles/Home.css'
 
 export const Home = () => {
     return (
-        <div></div>
+        <div className="body">
+        </div>
     );
 }
