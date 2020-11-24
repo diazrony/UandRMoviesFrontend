08b3674923e11b91b05c956f32f984ae7d63a0b9
Jesus Uriel Garrido Granados <urielgarridogra@gmail.com>
2020-11-23 23:38:19 -0600

    Login ahora si listo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import user from '../img/user.png';
 
 export const Home = () => {
     return (
-        <div className="body">
-            <div className="container">
-                <h1>Inicia Sesión</h1>
-                <img src={user}></img>
-                <input className="usuario" type="text" placeholder="Ingresa tu usuario"></input>
-                <input className="contraseña" type="password" placeholder="  Ingresa tu contraseña"></input>
-                <input className="boton" type="button" value="Iniciar Sesión"></input>
-            </div>
-        </div>
-    )
+        <div></div>
+    );
 }
