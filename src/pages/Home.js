2a4f89b8e7e6e53d723ec3b7f03fbe2e13e464e9
Jesus Uriel Garrido Granados <urielgarridogra@gmail.com>
2020-11-24 18:53:30 -0600

    Actualizacion con catalogo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,24 @@ import './styles/Home.css'
 
 export const Home = () => {
     return (
-        <div className="home">
+        <div className="section">
+            <div className="pelicula">
+                <img src="https://pics.filmaffinity.com/Joker-790658206-mmed.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg"></img>
+            </div>
+            <div className="pelicula"></div>
+                <img src=""></img>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
+            <div className="pelicula"></div>
         </div>
     );
 }
