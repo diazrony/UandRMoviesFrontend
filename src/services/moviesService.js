5b5757ebff4e24741d87f600034d35790a32224a
ronyd <ronydiaz99@gmail.com>
2020-11-24 23:11:04 -0600

    Cambios a home

---
+++ src/services/moviesService.js
@@ -0,0 +1,53 @@
+const movies = [
+    {
+        title: 'pelicula1',
+        img: 'https://k60.kn3.net/taringa/0/A/D/B/2/D/ivandgp73/C9A.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula2',
+        img: 'https://k60.kn3.net/taringa/F/4/D/C/6/8/ivandgp73/3D3.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula3',
+        img: 'https://www.ecartelera.com/carteles/900/914/003_m.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula4',
+        img: 'https://es.web.img3.acsta.net/pictures/17/11/08/09/46/1315249.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula5',
+        img: 'https://k60.kn3.net/taringa/9/5/6/9/9/A/ivandgp73/0C8.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula6',
+        img: 'https://es.web.img3.acsta.net/pictures/17/05/22/10/23/162388.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula7',
+        img: 'https://i.pinimg.com/736x/6d/3d/2f/6d3d2f70e70b4582842e0805907a13b4.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula8',
+        img: 'https://i.pinimg.com/originals/dd/16/12/dd1612ee7da90e05fb5b5b3ba208e2a1.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula9',
+        img: 'https://www.cinemascomics.com/wp-content/uploads/2016/03/Capit%C3%A1n-Am%C3%A9rica-Civil-War-poster.jpg',
+        description: 'Buena pelicula',
+    },
+    {
+        title: 'pelicula10',
+        img: 'https://es.web.img3.acsta.net/medias/nmedia/18/77/86/82/20486826.jpg',
+        description: 'Buena pelicula',
+    }
+]
+module.exports = movies;
