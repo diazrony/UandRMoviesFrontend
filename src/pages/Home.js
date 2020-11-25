19472cb24561d1dcf3420bd06a464f2b0ec18ad1
Jesus Uriel Garrido Granados <urielgarridogra@gmail.com>
2020-11-24 19:36:55 -0600

    Catalogo Responsivo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,17 +10,36 @@ export const Home = () => {
             <div className="pelicula">
                 <img src="https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg"></img>
             </div>
-            <div className="pelicula"></div>
-                <img src=""></img>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
-            <div className="pelicula"></div>
+            <div className="pelicula">
+                <img src="https://www.ecartelera.com/carteles/900/914/003_m.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://es.web.img3.acsta.net/pictures/17/11/08/09/46/1315249.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://pics.filmaffinity.com/Aquaman-355320736-large.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://es.web.img3.acsta.net/pictures/17/05/22/10/23/162388.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://i.pinimg.com/736x/6d/3d/2f/6d3d2f70e70b4582842e0805907a13b4.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://i.pinimg.com/originals/dd/16/12/dd1612ee7da90e05fb5b5b3ba208e2a1.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://www.cinemascomics.com/wp-content/uploads/2016/03/Capit%C3%A1n-Am%C3%A9rica-Civil-War-poster.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://es.web.img3.acsta.net/medias/nmedia/18/77/86/82/20486826.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://www.cinemascomics.com/wp-content/uploads/2019/05/poster-spider-man-lejos-de-casa-05.jpg"></img>
+            </div>
+            <div className="pelicula">
+                <img src="https://es.web.img3.acsta.net/pictures/17/03/09/14/11/154943.jpg"></img>
+            </div>
         </div>
     );
 }
