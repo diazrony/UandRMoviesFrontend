5b17b9b0a112773b72b69ee196a74880e5e3e3ff
Jesus Uriel Garrido Granados <urielgarridogra@gmail.com>
2020-11-25 22:35:28 -0600

    Create Movie hecho con responsive

--- src/pages/admin/CreateMovie.js
+++ src/pages/admin/CreateMovie.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import '../styles/CreateMovie.css';
 
 export const CreateMovie = () => {
     return (
-        <div>
-            
+        <div className="containerCreateMovie">
+            <div className="formulario">
+                <h1><b>Formulario Peliculas</b></h1>
+                <div className="formpeli">
+                    <p><b>Imagen</b></p>
+                    <input type="file" />
+                </div>
+                <div className="formpeli">
+                    <p><b>Nombre Pelicula</b></p>
+                    <input type="text" />
+                </div>
+                <div className="formpeli">
+                <p><b>Genero</b></p>
+                    <select name="genero">
+                        <option>Terror</option>
+                        <option>Drama</option>
+                        <option>Comedia</option>
+                        <option>Accion</option>
+                    </select>
+                </div>
+                <div className="formpeli">
+                    <p><b>Url</b></p>
+                    <input type="text" />
+                </div>
+                <br/><br/>
+                <div className="formpeli">
+                    <button>INGRESAR</button>
+                </div>
+            </div>
         </div>
     )
 }
